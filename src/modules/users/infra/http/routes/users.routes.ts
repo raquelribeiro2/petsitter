0c55821a06afbacbc00530f3394d8769bf29d20b
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-02-05 22:12:18 -0300

    Refactoring the user module for DDD implementation

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import { getRepository } from 'typeorm';
 import multer from 'multer';
 
-import uploadConfig from '../config/upload';
+import uploadConfig from '@config/upload';
 
-import CreateUserService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
+import User from '@modules/users/infra/typeorm/entities/User';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
-import User from '../models/User';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -24,7 +25,8 @@ usersRouter.get('/', ensureAuthenticated, async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
 
-  const createUser = new CreateUserService();
+  const usersRepository = new UsersRepository();
+  const createUser = new CreateUserService(usersRepository);
 
   const user = await createUser.execute({
     name,
@@ -42,7 +44,8 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
+    const usersRepository = new UsersRepository();
+    const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
 
     const user = await updateUserAvatar.execute({
       user_id: request.user.id,
