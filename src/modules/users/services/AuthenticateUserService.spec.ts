ff973cd67341faaf0de28b0dea32f1115adf90e2
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-02-19 09:23:55 -0300

    Implementation of authentication test when email and password do not match

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -35,27 +35,48 @@ describe('AuthenticateUser', () => {
     expect(response.user).toEqual(user);
   });
 
-  // it('should not be able to create a new user with same email from another', async () => {
-  //   const fakeUsersRepository = new FakeUsersRepository();
-  //   const fakeHashProvider = new FakeHashProvider();
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-  //   const createUser = new CreateUserService(
-  //     fakeUsersRepository,
-  //     fakeHashProvider,
-  //   );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
-  //   await createUser.execute({
-  //     name: 'John Doe',
-  //     email: 'johndoe@example.com',
-  //     password: '123456',
-  //   });
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 
-  //   expect(
-  //     createUser.execute({
-  //       name: 'John Doe',
-  //       email: 'johndoe@example.com',
-  //       password: '123456',
-  //     }),
-  //   ).rejects.toBeInstanceOf(AppError);
-  // });
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'johndoe@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
